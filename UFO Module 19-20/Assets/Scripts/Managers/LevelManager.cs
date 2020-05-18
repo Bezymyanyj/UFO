df4651b39ff06d953c34ca13ca64cece83100d0c
Bezymyanyj <Bezymyanyj@gmail.com>
2020-05-18 19:15:07 -0400

    Managers

---
+++ UFO Module 19-20/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour, IGameManager
+{
+    public ManagerStatus status {get; private set;}
+
+    public int CurrentLevel{get; private set;}
+    
+    public void Startup(){
+        Debug.Log("Player manager starting...");
+
+        CurrentLevel = SceneManager.GetActiveScene().buildIndex;
+
+        status = ManagerStatus.Started;
+    }
+
+
+    public void Restart(){
+        SceneManager.LoadScene(CurrentLevel);
+    }
+
+    public void LoadNextLevel(){
+        SceneManager.LoadScene(++CurrentLevel);
+    }
+}
