06b48ae3e62f2ee57351ee2c11c96216db7f1944
Bezymyanyj <Bezymyanyj@gmail.com>
2020-05-20 13:51:35 -0400

    Project2

---
+++ UFO/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public float cameraSpeed = 1;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position;
+    }
+    void LateUpdate()
+    {
+        // set the target object to follow
+        Transform target = player.transform;
+
+        //move towards the game object that is the target
+        float step = cameraSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position + offset, step);
+    }
+}
