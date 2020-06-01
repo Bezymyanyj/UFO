1f3c52f664e74d6a41f1bb3e6947bb97ade888d4
Bezymyanyj <Bezymyanyj@gmail.com>
2020-06-01 08:36:25 -0400

    Main menu script

---
+++ UFO/Assets/Scripts/UI_MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_MainMenu : MonoBehaviour
+{
+    public GameObject mainMenuWindow;
+    public GameObject optionsWindow;
+
+    public void StartGame(){
+        Messenger.Broadcast("Next_Level");
+    }
+    public void ExitApplication(){
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void OpenOptions(){
+        mainMenuWindow.SetActive(false);
+        optionsWindow.SetActive(true);
+    }
+}
