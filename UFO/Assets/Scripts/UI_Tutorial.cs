99d11ef0dab03191788c12b4ee35352faf5e3688
Bezymyanyj <Bezymyanyj@gmail.com>
2020-06-01 15:49:35 -0400

    UI Tutorial

---
+++ UFO/Assets/Scripts/UI_Tutorial.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Tutorial : MonoBehaviour
+{
+    public GameObject tutorialWindow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!Managers.Level.IsTutorialComplete){
+            Time.timeScale = 0;
+        }
+        else{
+            tutorialWindow.SetActive(false);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame(){
+        tutorialWindow.SetActive(false);
+        Managers.Level.IsTutorialComplete = true;
+        Time.timeScale = 1;
+    }
+}
