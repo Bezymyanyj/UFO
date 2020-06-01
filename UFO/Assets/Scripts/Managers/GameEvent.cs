7adb844c4e3c7cac6e25c6131530f4e1a6d6d5c9
Bezymyanyj <Bezymyanyj@gmail.com>
2020-06-01 08:38:00 -0400

    Messengers system

---
+++ UFO/Assets/Scripts/Managers/GameEvent.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameEvent {   
+    public const string Level_Failed = "Level_Failed";
+    public const string Next_Level = "Next_Level";
+    }
--- UFO/Assets/Scripts/Managers/LevelManager.cs
+++ UFO/Assets/Scripts/Managers/LevelManager.cs
@@ -8,21 +8,52 @@ public class LevelManager : MonoBehaviour, IGameManager
     public ManagerStatus status {get; private set;}
 
     public int CurrentLevel{get; private set;}
+
+    private bool isRestart;
     
     public void Startup(){
         Debug.Log("Player manager starting...");
 
         CurrentLevel = SceneManager.GetActiveScene().buildIndex;
 
+        isRestart = false;
+
         status = ManagerStatus.Started;
     }
 
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        Messenger.AddListener("Level_Failed", OnFailed);
+        Messenger.AddListener("Next_Level", LoadNextLevel);
+    }
+    /// <summary>
+    /// This function is called when the MonoBehaviour will be destroyed.
+    /// </summary>
+    void OnDestroy()
+    {
+        Messenger.RemoveListener("Level_Failed", OnFailed);
+        Messenger.RemoveListener("Next_Level", LoadNextLevel);
+    }
 
-    public void Restart(){
+    private void OnFailed(){
+        if(!isRestart){
+            StartCoroutine(Restart());
+        }
+    }
+
+    private IEnumerator Restart(){
+
+        isRestart = true;
+
+        yield return new WaitForSeconds(1);
+
         SceneManager.LoadScene(CurrentLevel);
     }
 
-    public void LoadNextLevel(){
+    private void LoadNextLevel(){
         SceneManager.LoadScene(++CurrentLevel);
     }
 }
