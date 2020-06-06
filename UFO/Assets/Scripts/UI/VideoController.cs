fe87b3685f57d3bb490816c73ddf721d934297d5
Bezymyanyj <Bezymyanyj@gmail.com>
2020-06-06 17:43:20 -0400

    VideoController

---
+++ UFO/Assets/Scripts/UI/VideoController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Michsky.UI.ModernUIPack;
+using UnityEngine;
+
+public class VideoController : MonoBehaviour
+{
+    private int _currentResolutionIndex;
+    
+    public CustomDropdown ResolutionDropdown;
+    
+    
+    public void SetFullScreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+    
+    public void SetResolution(int resolutionIndex)
+    {
+        int width = int.Parse(ResolutionDropdown.dropdownItems[resolutionIndex].itemName.Split('x')[0]);
+        int height = int.Parse(ResolutionDropdown.dropdownItems[resolutionIndex].itemName.Split('x')[1]);
+        
+        Screen.SetResolution(width, height, Screen.fullScreen);
+    }
+}
