8cb6fc35fee81847594d882ba570413a8a2d99e0
Bezymyanyj <Bezymyanyj@gmail.com>
2020-06-12 11:03:43 -0400

    Finish particle

---
+++ UFO/Assets/Scripts/FinishParticles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishParticles : MonoBehaviour
+{
+    public ParticleSystem[] fireWorks;
+
+    private void Awake()
+    {
+        Messenger.AddListener(GameEvent.Level_Complete, StartParticles);
+    }
+
+    private void OnDestroy()
+    {
+        Messenger.RemoveListener(GameEvent.Level_Complete, StartParticles);
+    }
+
+    private void StartParticles()
+    {
+        for (int i = 0; i < fireWorks.Length; i++)
+        {
+            fireWorks[i].Play();
+        }
+    }
+}
